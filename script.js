fe889ce755bf3bab27f159b38362be28d24beb3b
Ashutosh Tiwari <ashutosh122002@gmail.com>
2024-04-16 23:22:25 +0530

    full commit

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ window.onload = function (){
     context = board.getContext("2d");
 }
 //draw initial dinosaur
-context.fillStyle="red";
+context.fillStyle="green";
 context.fillRect(dinoX,dinoY,dino.width, dino.height);
 
 dinoImg= new Image();

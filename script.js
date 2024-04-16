d86d6f1f756af8ccecf600bc6a301c779e369122
Ashutosh Tiwari <ashutosh122002@gmail.com>
2024-04-17 00:13:28 +0530

    dragon

--- script.js
+++ script.js
@@ -1,49 +1,47 @@
 //board
 let board;
-let boardWidth= 850;
-let boardHeight= 250;
-let context ;
+let boardWidth = 850;
+let boardHeight = 250;
+let context;
 
 //dino
-let dinoWidth= 99;
-let dinoheight= 88;
-let dinoX= 50;
-let dinoY= boardHeight-dinoHeight ;
+let dinoWidth = 99;
+let dinoHeight = 88;
+let dinoX = 50;
+let dinoY = boardHeight - dinoHeight;
 let dinoImg;
 
 let dino = {
-    x : dinoX,
-    y : dinoY,
+    x: dinoX,
+    y: dinoY,
     height: dinoHeight,
     width: dinoWidth
-
 }
 
-window.onload = function (){
-    board= document.getElementById("board");
-    board.height= boardHeight;
-    board.width= boardWidth;
+window.onload = function () {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
 
     context = board.getContext("2d");
-}
-//draw initial dinosaur
-context.fillStyle="blue";
-context.fillRect(dinoX,dinoY,dino.width, dino.height);
 
-dinoImg= new Image();
-dinoImg.src="./img/dino.png/";
-dinoImg.onload= function(){
-context.drawImage(dino.Img, dino.x, dino.y,dino.width,dino.height);
+    //draw initial dinosaur
+    context.fillStyle = "blue";
+    context.fillRect(dinoX, dinoY, dino.width, dino.height);
+
+    dinoImg = new Image();
+    dinoImg.src = "./img/dino.png";
+    dinoImg.onload = function () {
+        context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+    }
 }
 
 //Cactus
-let cactusArray= [];
-let cactusHeight=78;
-let cactus1Width=44;
-let cactus2Width=80;
-let cactus3Width=100;
+let cactusArray = [];
+let cactusHeight = 78;
+let cactus1Width = 44;
+let cactus2Width = 80;
+let cactus3Width = 100;
 
-cactusX= 700;
-cactusY= boardHeight-cactusHeight;
-
-
+let cactusX = 700;
+let cactusY = boardHeight - cactusHeight;
